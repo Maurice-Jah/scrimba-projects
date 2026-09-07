8545a150e1a1f8be8a522d667dff73e7d6bec888
Maurice Jah <mauricejahwizwid@gmail.com>
2026-09-07 04:59:04 +0100

    Setup for chrome extension

---
+++ leadTracker/index.js
@@ -0,0 +1,19 @@
+let myLeads = [];
+const inputEl = document.getElementById('input-el');
+const ulEl = document.getElementById('ul-el');
+const inputBtn = document.getElementById('input-btn');
+
+// Display the Leads
+function renderLeads() {
+  let listItems = '';
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li> <a href="#" target='_blank'> ${myLeads[i]}</a></li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+inputBtn.addEventListener('click', function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = '';
+  renderLeads();
+});
