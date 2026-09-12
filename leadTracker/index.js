976a79b4c85182cb499a5e6069392dd3d589bae3
Maurice Jah <mauricejahwizwid@gmail.com>
2026-09-12 05:02:44 +0100

    Read from LocalStoarge

--- leadTracker/index.js
+++ leadTracker/index.js
@@ -12,8 +12,20 @@ function renderLeads() {
   ulEl.innerHTML = listItems;
 }
 
+// Read from localStorage
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
 inputBtn.addEventListener('click', function () {
   myLeads.push(inputEl.value);
   inputEl.value = '';
+
+  localStorage.setItem('myLeads', JSON.stringify(myLeads));
+
   renderLeads();
 });
