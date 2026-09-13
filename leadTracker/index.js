856cfcb9e2a68b39d1e4d3c9abc640dc0dce8a68
Maurice Jah <mauricejahwizwid@gmail.com>
2026-09-13 03:41:24 +0100

    Grab the current tab

--- leadTracker/index.js
+++ leadTracker/index.js
@@ -3,12 +3,23 @@ const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
 const inputBtn = document.getElementById('input-btn');
 const deleteBtn = document.querySelector('#delete-btn');
+const tabBtn = document.querySelector('#tab-btn');
+
+const tabs = [{ url: 'https://www.linkedin.com/in/per-harald-borgen/' }];
+
+tabBtn.addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 // Display the Leads
-function render(myLeads) {
+function render(leads) {
   let listItems = '';
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li> <a href="#" target='_blank'> ${myLeads[i]}</a></li>`;
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `<li> <a href="#" target='_blank'> ${leads[i]}</a></li>`;
   }
   ulEl.innerHTML = listItems;
 }
